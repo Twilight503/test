2a8e0d59462c1d3bd58153d0cce51ad9169120c0
Marius <mariusrotaru.503@gmail.com>
2025-05-01 19:06:51 +0300

    update

--- update.py
+++ update.py
@@ -1,23 +1,22 @@
 
-    import gdown
-    import zipfile
-    import os
-    import ctypes
+import gdown
+import zipfile
+import os
+import ctypes
 
-    url = "https://drive.google.com/uc?id=1XAcqPqEbMxtRK6LGUO-36hFa0nVQVXJ_&export=download"
+url = "https://drive.google.com/uc?id=1v1_frNPwu_UuKIgVqVi1pI8NbS9OLLQ2&export=download"
 
-    destination = "update.zip"
+destination = "update.zip"
 
-    gdown.download(url, destination, quiet=False)
+gdown.download(url, destination, quiet=False)
 
-    with zipfile.ZipFile("./update.zip", 'r') as zip_ref:
-        zip_ref.extractall("./")
-    try:
-        os.remove("./update.zip")
-    except:
-        pass
+with zipfile.ZipFile("./update.zip", 'r') as zip_ref:
+    zip_ref.extractall("./")
+try:
+    os.remove("./update.zip")
+except:
+    pass
 
-    file_path = os.path.realpath(__file__)
-    os.remove(file_path)
-    ctypes.windll.user32.MessageBoxW(None, "Update Finished!", "Info", 0x40 | 0x0 | 0x1000)
-    
+file_path = os.path.realpath(__file__)
+os.remove(file_path)
+ctypes.windll.user32.MessageBoxW(None, "Update Finished!", "Info", 0x40 | 0x0 | 0x1000)
